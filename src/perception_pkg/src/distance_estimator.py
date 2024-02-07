b860b45876246931d49b2406ecf5024cf6a4f80b
PedroSoler10 <soler.pedrojavier@gmail.com>
2024-02-07 18:22:23 +0100

    last update before sunday 4th

--- src/perception_pkg/src/distance_estimator.py
+++ src/perception_pkg/src/distance_estimator.py
@@ -1,60 +1,140 @@
-import cv2
-import imutils
-from ultralytics import YOLO
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import Bool, String, Float64
+from vision_msgs.msg import Detection2DArray, Detection2D, BoundingBox2D 
+
 import numpy as np
 import os
 import yaml
+import matplotlib.pyplot as plt
+from datetime import datetime
 
-# Read the YAML file
-yaml_file = os.path.join(os.getcwd(), 'launch/ost_2.yaml')
-with open(yaml_file, 'r') as file:
-    camera_parameters = yaml.safe_load(file)
+class DistanceEstimator:
+    def __init__(self):
+        # Initialize the ROS node
+        rospy.init_node('distance_estimator')
 
-# Extract camera matrix
-camera_matrix_data = camera_parameters['camera_matrix']['data']
-camera_matrix = np.array(camera_matrix_data).reshape(3, 3)
-focal_length_x = camera_matrix[0][0]
-focal_length_y = camera_matrix[1][1]
+        # Get parameters
+        detections_topic = rospy.get_param('detections_topic', '/detectnet/detections')
+        self.visualization_flag = rospy.get_param('distance_visualization_flag', True)
 
-# Extract distortion coefficients
-dist_coeffs_data = camera_parameters['distortion_coefficients']['data']
-dist_coeffs = np.array(dist_coeffs_data).reshape(-1, 1)
+        # Initialize the position estimator
+        self.init_pose_estimator()
 
-# Known object dimensions (in meters or any consistent unit)
-real_width = 0.50
-real_height = 1.80
+        # # Initialize the vectors for the plot
+        self.t = []
+        self.dist = []
 
-# Detected object dimensions in the image (in pixels)
-yolo_pretrained = os.path.join(os.getcwd(), 'src/perception_pkg/yolo_pretrained/yolov8n.pt')
-model = YOLO(yolo_pretrained)
-image_path = os.path.join(os.getcwd(), 'src/perception_pkg/images/person_standing.jpg')
-image = cv2.imread(image_path)
-image= imutils.rotate_bound(image, 90)  # Rotate 90 degrees
-results = model(image, classes=0, show=True)
-print("Results:\n---------------\n", results)
-print("Boxes:\n-----------------\n", results[0].boxes)
-print(len(results[0].boxes))
+        # Init thresholds
+        self.image_resolution = 720.0*1280.0    # 720*1080=777600
+        up_t_i = 0.3
+        low_t_i = 0.2
+        up_t_c = 0.8
+        low_t_c = 0.5
+        self.upper_importance_threshold = up_t_i * up_t_c       # 0.3*0.8=0.24
+        self.lower_importance_threshold = low_t_i * low_t_c     # 0.2*0.5=0.1
 
-# Detected object dimensions in the image (in pixels)
-image_width_pixels = results[0].boxes[0].xywh[0][2].item()
-image_height_pixels = results[0].boxes[0].xywh[0][3].item()
+        # Create a Publisher for the distance topic
+        self.publisher = rospy.Publisher('/distance', Float64, queue_size=10)
 
-# Calculate the distance using the width (or height)
-distance_width = (focal_length_x * real_width) / image_width_pixels
-distance_height = (focal_length_y * real_height) / image_height_pixels
+        # Create a Subscriber for the detections topic
+        self.subscriber = rospy.Subscriber(detections_topic, Detection2DArray, self.callback)
 
-# Average distance between width and height distances
-distance = (distance_width + distance_height) / 2  # or distance_height
-print("Distance from the camera", distance)
+        # Call the function plot_before_shutdown before the node dies
+        rospy.on_shutdown(self.plot_before_shutdown)
 
-# Compute world coordinates (assuming object is at Z=0)
-# world_x = undistorted_point[0]
-# world_y = undistorted_point[1]
-# world_z = 0  # Same height as the camera
+    def plot_before_shutdown(self):
+        print("...Function before shutdown running...")
+        print("......................................")
+        print("......................................")
+        
+        # Create the plot
+        plt.plot(self.t, self.dist)
+        # print("TIME:\n\n", self.t)
+        # print("DISTANCE:\n\n", self.dist)
+        plt.title('Object distance to the camera')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Distance (m)')
 
-# world_point = np.array([world_x, world_y, world_z])
+        # Save the plot as an image
+        # Get current date and time
+        current_time = datetime.now()
 
-# print("3D world point:", world_point)
+        # Format as a string suitable for a filename
+        formatted_time = current_time.strftime("%Y%m%d_%H%M%S")
 
-while True:
-    results = model(image, classes=0, verbose=False, show=True)
+        # Create filename
+        filename = "distance_plot_" + formatted_time + ".png"
+        plot_path = os.path.join(os.getcwd(), 'src/perception_pkg/plots/', filename)
+        plt.savefig(plot_path)
+
+        # Show directly the plot
+        # plt.show()
+
+    def init_pose_estimator(self):
+        # Read the YAML file
+        yaml_file = os.path.join(os.getcwd(), 'src/perception_pkg/camera_calibration/c505e.yaml')
+        with open(yaml_file, 'r') as file:
+            camera_parameters = yaml.safe_load(file)
+
+        # Extract camera matrix
+        camera_matrix_data = camera_parameters['camera_matrix']['data']
+        camera_matrix = np.array(camera_matrix_data).reshape(3, 3)
+        self.focal_length_x = camera_matrix[0][0]
+        self.focal_length_y = camera_matrix[1][1]
+
+        # Known object dimensions (in meters or any consistent unit)
+        self.real_width = 0.50
+        self.real_height = 1.80
+
+    def callback(self, data):
+        
+        main_human_detection = Detection2D()
+        max_importance = 0.0
+
+        for detection in data.detections:
+            if detection.results[0].id == 1:
+                print("Human detected")
+                importance = detection.bbox.size_x * detection.bbox.size_y * detection.results[0].score / self.image_resolution
+                if importance > max_importance:
+                    print("Pedestrian detected")
+                    max_importance = importance
+                    main_human_detection = detection
+        
+        if max_importance > self.lower_importance_threshold:
+            # Detected object dimensions in the image (in pixels)
+            image_width_pixels = main_human_detection.bbox.size_x
+            image_height_pixels = main_human_detection.bbox.size_y
+
+            # Calculate the distance using the width (or height)
+            distance_width = (self.focal_length_x * self.real_width) / image_width_pixels
+            distance_height = (self.focal_length_y * self.real_height) / image_height_pixels
+
+            # Average distance between width and height distances
+            # distance = (distance_width + distance_height) / 2
+            self.distance = distance_width
+            print("Distance from the camera", self.distance)
+
+            # Publish the distance
+            self.publisher.publish(self.distance)
+            rospy.loginfo("Published: %s", self.distance)
+
+            if self.visualization_flag:
+                # Store the time and distance values for the plot
+                current_time = rospy.Time.now()
+                current_time_float = current_time.to_sec()
+                self.t.append(current_time_float)
+                self.dist.append(self.distance)
+        else:
+            print("No objects detected.")
+            
+    def run(self):
+        # Keep the node running
+        rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        node = DistanceEstimator()
+        node.run()
+    except rospy.ROSInterruptException:
+        pass
