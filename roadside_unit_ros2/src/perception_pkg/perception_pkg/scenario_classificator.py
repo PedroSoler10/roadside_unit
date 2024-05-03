5cb5142f18ce3691c2483723205343bdbe5a4fcc
PedroSoler10 <soler.pedrojavier@gmail.com>
2024-05-03 18:22:27 +0200

    fixes and flip

--- roadside_unit_ros2/src/perception_pkg/perception_pkg/scenario_classificator.py
+++ roadside_unit_ros2/src/perception_pkg/perception_pkg/scenario_classificator.py
@@ -10,9 +10,9 @@ class ScenarioClassificator(Node):
         super().__init__('scenario_classificator')
 
         detections_topic = self.declare_parameter('detections_topic', '/detectnet/detections').value
-        self.visualization_flag = self.declare_parameter('scenario_visualization_flag', True).value
+        self.visualization_flag = self.declare_parameter('scenario_visualization_flag', False).value
 
-        self.publisher = self.create_publisher(String, '/scenario', 10)
+        self.scenario_publisher = self.create_publisher(String, '/scenario', 10)
 
         # Init thresholds
         self.image_resolution = 720.0 * 1280.0
@@ -27,7 +27,8 @@ class ScenarioClassificator(Node):
         self.time, self.width, self.height, self.size, self.conf, self.imp = [], [], [], [], [], []
 
         self.scenario = "unknown"
-        self.subscriber = self.create_subscription(Detection2DArray, detections_topic, self.callback, 10)
+        self.scenario_publisher.publish(String(data=self.scenario))
+        self.detections_subscriber = self.create_subscription(Detection2DArray, detections_topic, self.detections_callback, 10)
 
     def visualize_metrics(self, detection, stamp):
         self.time.append(stamp)
@@ -38,10 +39,9 @@ class ScenarioClassificator(Node):
         self.imp.append(detection.bbox.size_x * detection.bbox.size_y * detection.results[0].score / self.image_resolution)
         self.get_logger().info(f"time:\t{stamp:.2f}\twidth:\t{self.width[-1]:.0f}\theight:\t{self.height[-1]:.0f}\tsize:\t{self.size[-1]:.0f}\tconf:\t{self.conf[-1]:.4f}\timp:\t{self.imp[-1]:.4f}")
 
-    def callback(self, data):
+    def detections_callback(self, data):
         main_human_detection = Detection2D()
-        max_importance = -1.0
-
+        max_importance = 0.0
         for detection in data.detections:
             if detection.results[0].id == "\x01":
                 self.get_logger().info("Human detected")
@@ -55,13 +55,13 @@ class ScenarioClassificator(Node):
             self.scenario = "used"
         elif max_importance > self.lower_importance_threshold:
             self.scenario = "solicited"
-        elif max_importance >= 0:
+        else:
             self.scenario = "free"
 
-        self.publisher.publish(String(data=self.scenario))
+        self.scenario_publisher.publish(String(data=self.scenario))
         self.get_logger().info('Published: %s' % self.scenario)
 
-        if self.visualization_flag and self.scenario != "free" and self.scenario != "unknown":
+        if self.visualization_flag and (self.scenario != "free" or self.scenario != "unknown"):
             self.visualize_metrics(main_human_detection, data.header.stamp.sec)
 
 def main(args=None):
